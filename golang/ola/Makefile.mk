f8a9af58254682f8a72467ef1f1092fe69a3cc29
SeanSill <sms3h2@gmail.com>
2015-03-29 10:41:45 -0400

    Fixed a small makefile error

--- golang/ola/Makefile.mk
+++ golang/ola/Makefile.mk
@@ -1,7 +1,7 @@
 # Build the go client
 
 built_sources += golang/ola/Ola.pb.go \
-								 golang/ola/Service.pb.go \
+								 golang/ola/OlaService.pb.go \
 								 golang/ola/Rpc.pb.go
 
 golang/ola/Ola.pb.go: golang/ola/Makefile.mk common/protocol/Ola.proto
@@ -10,5 +10,5 @@ golang/ola/Ola.pb.go: golang/ola/Makefile.mk common/protocol/Ola.proto
 golang/ola/Rpc.pb.go: golang/ola/Makefile.mk common/rpc/Rpc.proto
 	$(PROTOC) --go_out golang/ola --proto_path $(srcdir)/common/rpc $(srcdir)/common/rpc/Rpc.proto
 
-golang/ola/Service.pb.go: golang/ola/Makefile.mk common/protocol/Ola.proto protoc/ola_protoc_golang_plugin$(EXEEXT)
+golang/ola/OlaService.pb.go: golang/ola/Makefile.mk common/protocol/Ola.proto protoc/ola_protoc_golang_plugin$(EXEEXT)
 	$(OLA_PROTOC) --goservice_out golang/ola --proto_path $(srcdir)/common/protocol $(srcdir)/common/protocol/Ola.proto
