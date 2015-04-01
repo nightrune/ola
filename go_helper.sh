44e8b4e4b7cb79a8df333e1eac825fab62bacfe1
SeanSill <sms3h2@gmail.com>
2015-03-31 20:51:18 -0400

    Attempting to build a go workspace withing golang

--- go_helper.sh
+++ go_helper.sh
@@ -3,7 +3,7 @@
 # This needs to be generated to set the export path correctly
 
 OLD_GOPATH=$GOPATH
-export GOPATH=/home/sean/ola/golang/gocode
+export GOPATH=/home/sean/ola/golang/
 go build $@
 rc=$?
 export GOPATH=$OLD_GOPATH
