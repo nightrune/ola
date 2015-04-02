70ba63b7cafd8e7431d673adc1f91fa96ab79363
SeanSill <sms3h2@gmail.com>
2015-04-01 22:07:39 -0400

    Fully auto go build system now

--- golang/examples/Makefile.mk
+++ golang/examples/Makefile.mk
@@ -10,6 +10,6 @@ built_sources += \
     golang/examples/ola_send_dmx
 
 golang/examples/ola_send_dmx: golang/examples/ola_send_dmx.go
-	./go_helper.sh -o golang/examples/ola_send_dmx golang/examples/ola_send_dmx.go
+	$(OLA_GO) -o golang/examples/ola_send_dmx golang/examples/ola_send_dmx.go
 
 endif
