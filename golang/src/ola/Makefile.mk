71301609dc1085ae78364e18828f3f963cfae2bd
SeanSill <sms3h2@gmail.com>
2015-04-01 22:18:26 -0400

    Fixed a test with the makefile

--- golang/src/ola/Makefile.mk
+++ golang/src/ola/Makefile.mk
@@ -6,10 +6,10 @@ built_sources += golang/src/ola/ola_proto/Ola.pb.go \
 								 golang/src/ola/OlaService.pb.go \
 								 golang/src/ola/ola_rpc/Rpc.pb.go
 
-$(SRCDIR)/ola_proto/Ola.pb.go: golang/src/ola/Makefile.mk common/protocol/Ola.proto
+golang/src/ola/ola_proto/Ola.pb.go: golang/src/ola/Makefile.mk common/protocol/Ola.proto
 	$(PROTOC) --go_out golang/src/ola/ola_proto --proto_path $(srcdir)/common/protocol $(srcdir)/common/protocol/Ola.proto
 
-$(SRCDIR)/ola_rpc/Rpc.pb.go: golang/src/ola/Makefile.mk common/rpc/Rpc.proto
+golang/src/ola/ola_rpc/Rpc.pb.go: golang/src/ola/Makefile.mk common/rpc/Rpc.proto
 	$(PROTOC) --go_out golang/src/ola/ola_rpc --proto_path $(srcdir)/common/rpc $(srcdir)/common/rpc/Rpc.proto
 
 golang/src/ola/OlaService.pb.go: golang/src/ola/Makefile.mk common/protocol/Ola.proto protoc/ola_protoc_golang_plugin$(EXEEXT)
